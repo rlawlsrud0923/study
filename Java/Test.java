deb9eadf297f1276d531d47715d7fa5e5584f935
rlawlsrud0923 <rlawlsrud0923@gmail.com>
2017-01-07 23:15:33 +0900

    20170107

---
+++ Java/Test.java
@@ -0,0 +1,13 @@
+//파일명 : Test.java
+//Java 확장자 - .java
+class Test
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+  }
+}
+/*
+번역방법: javac Test.java  ->컴파일하기
+실행방법: java Test
+*/
