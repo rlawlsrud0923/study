cd5c198547e2cbc930c90c7717cffad666395805
rlawlsrud <rlawlsrud0923@example.com>
2017-02-16 14:02:27 +0900

    2017.02.16

--- dean/js/news.js
+++ dean/js/news.js
@@ -1,3 +1,3 @@
-if () {
-
-}
+// (function($) {
+//   $('head').append('<style type="text/css">#qLpercentage {font-family: texgyreadventorregular;font-size: 45px;text-transform: uppercase;line-height: 55px;letter-spacing: 2px;color: #ffffff;}</style>');
+// })(jQuery);
