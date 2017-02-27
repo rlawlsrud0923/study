b47b93dcb37accd04073950b858332c711db0e37
rlawlsrud <rlawlsrud0923@example.com>
2017-02-27 14:03:52 +0900

    2017.02.27

--- sandellsandberg/js/btn.js
+++ sandellsandberg/js/btn.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
-  $(".fadebanner ul li a").click(function () { //lnb_logo 클릭하면
-    $(this).css("color","#FCED22");
+
+  //fadebanner btn mouseover
+  $(".fadebanner ul li a,.fadebanner button")
+  .mouseover(function () {
+    $(this).css("color","rgba(0,0,0,0.7)");
+    $(this).css("background-color","rgba(252,237,35,0.9)");
+  })
+  .mouseout(function() {
+    $(".fadebanner ul li a").css("background-color","#FFF");
+
+    $(".fadebanner button").css({"color":"#FFF","background-color":"rgba(0,0,0,0.7)"});
+  });
+
+
+  //kontakt btn
+  $("#kontakt button:nth-child(1)")
+  .css({"color":"#000","background":"#dbdbdb"});
+
+
+  $("#kontakt button").not("#kontakt button:nth-child(1)")
+  .mouseover(function () {
+    $(this).css({"color":"#000","background-color":"#dbdbdb"});
+  })
+  .mouseout(function() {
+    $("#kontakt button").css({"color":"#FFF","background-color":"#0c0c0c"});
   });
 });
