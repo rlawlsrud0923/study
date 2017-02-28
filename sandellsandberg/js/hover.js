0842f1b5342d816b778cca83ce24f42408da197e
rlawlsrud <rlawlsrud0923@example.com>
2017-02-28 11:33:50 +0900

    2017.02.28

--- sandellsandberg/js/hover.js
+++ sandellsandberg/js/hover.js
@@ -1,15 +1,15 @@
 $(function(){
    //문서가 준비되면
 
-   $( ".projekt_article li" ).find("a").css("opacity","0");
-
-   $( ".projekt_article li" ).hover(
-     function() {
-       $( ".projekt_article li a" ).css("opacity","1");
-     }, function() {
-       $( ".projekt_article li img" ).css("opacity","0.7");
-     }
-   );
+  //  $( ".projekt_article li a" ).find("h4").css("opacity","0");
+   //
+  //  $( ".projekt_article li" ).hover(
+  //    function() {
+  //      $( ".projekt_article li a" ).css("opacity","1");
+  //    }, function() {
+  //      $( ".projekt_article li img" ).css("opacity","0.7");
+  //    }
+  //  );
 
   //  $( "li.fade" ).hover(function() {
   //    $( this ).fadeOut( 100 );
