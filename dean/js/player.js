deb5fdcdb569edfcf97816fbc3dd0c6b7f6dbc7c
rlawlsrud <rlawlsrud0923@example.com>
2017-03-03 18:23:54 +0900

    2017.03.03

--- dean/js/player.js
+++ dean/js/player.js
@@ -1,7 +1,32 @@
 $( document ).ready( function() {
 
-  $( "#plus" ).click({
-    $("#play_wrap").popUp();
-  })
+  //
+  // $("#plus").click(function(){
+  //   $("#player_wrap").animate({bottom: '-100px'},"fast", function(){
+  //     $("#plus").click(function(){
+  //       $("#player_wrap").animate({bottom: '-171px'},"fast")
+  //     });
+  //   },function(){
+  //     $("#player_wrap").animate({bottom: '-171px'},"fast")
+  //   });
+  // });
+
+
+// $( "#plus" ).unbind({
+//   click: function() {
+//     $( "#player_wrap" ).animate({bottom: '-100px'},"fast")
+//   },
+// });
+
+
+
+$( "#plus").bind( "click", function( event ) {
+  $( "#player_wrap" ).animate( {bottom: '-100px'},"fast");
+});
+$( "#plus").bind( "click", function( event ) {
+  $( "#player_wrap" ).animate( {bottom: '-171px'},"fast");
+});
+
+
 
 });
