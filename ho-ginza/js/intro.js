69d2ac7ddf55da0d2521f326b1e7f9508642c4ab
rlawlsrud0923 <rlawlsrud0923@gmail.com>
2017-03-22 17:25:21 +0900

    2017 / 03 / 22

---
+++ ho-ginza/js/intro.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  //intro
+  $(window).load(function(){
+    $('.intro').animate({
+     opacity:0
+   }, {
+      duration: 3000,
+      complete: function () {
+      $('.intro').css("display", "none");
+      }
+    });
+  });
+
+  //logo 클릭 - 맨위로
+  $("#lnb_logo").click(function () { //lnb_logo 클릭하면
+    $('html,body').animate({scrollTop: 0}, 'normal'); //스크롤 top값 0으로
+    return false;
+  });
+
+  //icon_scroll 클릭 - 맨아래로
+  $("#icon_scroll").click(function () { //lnb_logo 클릭하면
+    $('html,body').animate({scrollTop: $('body').height()}, 'normal'); //스크롤 top값 0으로
+    return false;
+  });
+
+
+
+
+});
